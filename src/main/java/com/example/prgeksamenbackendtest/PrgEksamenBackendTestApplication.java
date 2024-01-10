faa6c9bc282336a05faf426a6261f0c3fb474720
danieljappe <danieljappe@icloud.com>
2024-01-10 17:09:05 +0100

    Opsat backend med security

---
+++ src/main/java/com/example/prgeksamenbackendtest/PrgEksamenBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.prgeksamenbackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrgEksamenBackendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrgEksamenBackendTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/prgeksamenbackendtest/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.prgeksamenbackendtest.user;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
